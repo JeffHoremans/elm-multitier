c3358616afb00cc82786238e1a6287ef2765776d
JeffHoremans <horemansjeff@gmail.com>
2017-02-09 07:18:55 +0100

    Added effects on rpc map

--- src/Multitier/RPC.elm
+++ src/Multitier/RPC.elm
@@ -17,11 +17,14 @@ type alias Handler msg = Result Error Value -> msg
 mapHandlers : (a -> msg) -> Handler a -> Handler msg
 mapHandlers f handler = handler >> f
 
-mapUpdate : (b -> serverMsg) -> (a -> serverModel -> serverModel) -> (serverModel -> a) -> (a -> (a, Task Error Value, Cmd b)) -> (serverModel -> (serverModel, Task Error Value, Cmd serverMsg))
-mapUpdate toServerMsg updateModel toA toTask = \serverModel -> let a = (toA serverModel) in
-                                                let (newA, task, cmd) = toTask a in (updateModel newA serverModel, task, Cmd.map toServerMsg cmd)
+mapUpdate : (b -> serverMsg) -> (a -> serverModel -> (serverModel, Cmd serverMsg)) -> (serverModel -> a) -> (a -> (a, Task Error Value, Cmd b)) -> (serverModel -> (serverModel, Task Error Value, Cmd serverMsg))
+mapUpdate toServerMsg updateModel toA toTask = \serverModel ->
+  let a = (toA serverModel) in
+    let (newA, task, cmd) = toTask a in
+      let (newModel, newCmd) = updateModel newA serverModel in
+        (newModel, task, Cmd.batch [newCmd, Cmd.map toServerMsg cmd])
 
-map : (b -> msg) -> (c -> serverMsg) -> (a -> serverModel -> serverModel) -> (serverModel -> a) -> RPC a b c -> RPC serverModel msg serverMsg
+map : (b -> msg) -> (c -> serverMsg) -> (a -> serverModel -> (serverModel, Cmd serverMsg)) -> (serverModel -> a) -> RPC a b c -> RPC serverModel msg serverMsg
 map toMsg toServerMsg fromA toA (Rpc handlers update) = Rpc (mapHandlers toMsg handlers) (mapUpdate toServerMsg fromA toA update)
 
 rpc : (Result Error result -> msg) -> (serverModel -> (serverModel, Task Error result, Cmd serverMsg)) -> RPC serverModel msg serverMsg
