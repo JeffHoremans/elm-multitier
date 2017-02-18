f7335a9d27ce6d06c6977751bca6ae47a9524b96
JeffHoremans <horemansjeff@gmail.com>
2017-02-18 02:24:11 +0100

    logtest (2)

--- src/Native/Multitier/Server/HttpServer/LowLevel.js
+++ src/Native/Multitier/Server/HttpServer/LowLevel.js
@@ -137,9 +137,9 @@ var _user$project$Native_Multitier_Server_HttpServer_LowLevel = function() {
       }
 
       var broadcast = function(path, message) {
-        console.log(path);
-        console.log(message);
         return Scheduler.nativeBinding(function(callback) {
+          console.log(mounted);
+          console.log(active_connections)
           if(mounted[path] === true){
             for (var id in active_connections[path]){
               active_connections[path][id].sendUTF(message);
